2374258dce4e590aa07407af6ec558a70ed2b84c
jieli <zmctinas@163.com>
2022-08-24 11:09:40 +0800

    week 10

--- road-to-lens/apollo-client.js
+++ road-to-lens/apollo-client.js
@@ -5,6 +5,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
     uri: "https://api.lens.dev",
     cache: new InMemoryCache(),
+    
 });
 
 export default client;
